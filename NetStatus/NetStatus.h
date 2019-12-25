1f0fe3c5fb370f4610860d7970c663af4043e7ef
Sushant Jagtap <sushantjagtap9945@gmail.com>
2019-12-25 22:58:52 +0530

    Added framework source and podspec

---
+++ NetStatus/NetStatus.h
@@ -0,0 +1,19 @@
+//
+//  NetStatus.h
+//  NetStatus
+//
+//  Created by Sushant Jagtap on 21/12/19.
+//  Copyright © 2019 Sushant Jagtap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetStatus.
+FOUNDATION_EXPORT double NetStatusVersionNumber;
+
+//! Project version string for NetStatus.
+FOUNDATION_EXPORT const unsigned char NetStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetStatus/PublicHeader.h>
+
+
